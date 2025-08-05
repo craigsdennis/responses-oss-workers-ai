c959ab280fff547abf88abe24628ddbfcd78fd63
Craig Dennis <craigsdennis@gmail.com>
2025-08-05 15:49:12 -0700

    Updates

--- worker/index.ts
+++ worker/index.ts
@@ -19,72 +19,27 @@ app.post("/api/examples/create", async (c) => {
   return c.json({ response, outputText: response.output_text });
 });
 
-app.post("/api/examples/create/streaming", async (c) => {
-  const { prompt } = await c.req.json();
-  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
-  const streamedResponse = await openai.responses.create({
+app.post("/api/examples/create/reasoning", async (c) => {
+  const { topic, effort } = await c.req.json();
+  const openai = new OpenAI({
+    apiKey: c.env.CLOUDFLARE_AUTH_TOKEN,
+    baseURL: c.env.BASE_URL,
+  });
+
+  const response = await openai.responses.create({
     model: MODEL_NAME,
-    input: prompt,
-    stream: true,
-  });
-  c.header("Content-Encoding", "Identity");
-  return streamText(c, async (stream) => {
-    for await (const event of streamedResponse) {
-      if (event.type === "response.output_text.delta") {
-        stream.write(event.delta);
-      }
-    }
-  });
-});
-
-app.post("/api/examples/create/stored", async (c) => {
-  const { word, previous_response_id } = await c.req.json();
-  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
-
-  const response = await openai.responses.create({
-    model: "gpt-4.1",
-    instructions: `You are playing a game of one word at a time. 
-        The user is going to respond with a single word that moves the story forward. 
-        Only reply with a single word and perhaps punctuation if it is the end of a sentence.`,
-    input: word,
-    store: true, //this is the default, being explicit
-    previous_response_id,
-  });
-  return c.json({
-    response,
-    previous_response_id: response.id,
-    word: response.output_text,
-  });
-});
-
-app.post("/api/examples/create/code-interpreter", async (c) => {
-  const { story } = await c.req.json();
-  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
-
-  const response = await openai.responses.create({
-    model: "gpt-4.1",
-    instructions: `Your job is to calculate how much time was spent based on free form text.
-      Then use that time to figure out how much it would've cost in US minimum wage of $15.
-      Respond to the user with how much time you estimated and total amount they would have been paid using the python tool.
+    instructions: `You help plan educational lessons.
+    The user will tell you they are trying to learn.
+    Your job is to create a detailed list of prerequisite skills and information, the user should ensure they know.
     `,
-    input: story,
-    include: ["code_interpreter_call.outputs"],
-    tool_choice: "required",
-    tools: [
-      {
-        type: "code_interpreter",
-        container: { type: "auto" },
-      },
-    ],
+    input: topic,
+    reasoning: {
+      effort,
+    },
   });
   return c.json({ response, outputText: response.output_text });
 });
 
-type Feedback = {
-  nps: number;
-  whatWorked?: string;
-  whatCouldBeImproved?: string;
-};
 function submitFeedback({ nps, whatWorked, whatCouldBeImproved }: Feedback) {
   console.log("Probably should save this to a database", {
     nps,
@@ -94,6 +49,7 @@ function submitFeedback({ nps, whatWorked, whatCouldBeImproved }: Feedback) {
   return { success: true, assignee: "Bob", submissionId: crypto.randomUUID() };
 }
 
+// TODO: Not working
 app.post("/api/examples/create/function-calling", async (c) => {
   const { feedback } = await c.req.json();
   const openai = new OpenAI({
@@ -174,6 +130,76 @@ app.post("/api/examples/create/function-calling", async (c) => {
   });
 });
 
+// TODO: Convert below here
+
+app.post("/api/examples/create/streaming", async (c) => {
+  const { prompt } = await c.req.json();
+  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+  const streamedResponse = await openai.responses.create({
+    model: MODEL_NAME,
+    input: prompt,
+    stream: true,
+  });
+  c.header("Content-Encoding", "Identity");
+  return streamText(c, async (stream) => {
+    for await (const event of streamedResponse) {
+      if (event.type === "response.output_text.delta") {
+        stream.write(event.delta);
+      }
+    }
+  });
+});
+
+app.post("/api/examples/create/stored", async (c) => {
+  const { word, previous_response_id } = await c.req.json();
+  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+
+  const response = await openai.responses.create({
+    model: "gpt-4.1",
+    instructions: `You are playing a game of one word at a time. 
+        The user is going to respond with a single word that moves the story forward. 
+        Only reply with a single word and perhaps punctuation if it is the end of a sentence.`,
+    input: word,
+    store: true, //this is the default, being explicit
+    previous_response_id,
+  });
+  return c.json({
+    response,
+    previous_response_id: response.id,
+    word: response.output_text,
+  });
+});
+
+app.post("/api/examples/create/code-interpreter", async (c) => {
+  const { story } = await c.req.json();
+  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+
+  const response = await openai.responses.create({
+    model: "gpt-4.1",
+    instructions: `Your job is to calculate how much time was spent based on free form text.
+      Then use that time to figure out how much it would've cost in US minimum wage of $15.
+      Respond to the user with how much time you estimated and total amount they would have been paid using the python tool.
+    `,
+    input: story,
+    include: ["code_interpreter_call.outputs"],
+    tool_choice: "required",
+    tools: [
+      {
+        type: "code_interpreter",
+        container: { type: "auto" },
+      },
+    ],
+  });
+  return c.json({ response, outputText: response.output_text });
+});
+
+type Feedback = {
+  nps: number;
+  whatWorked?: string;
+  whatCouldBeImproved?: string;
+};
+
+
 app.post("/api/examples/create/character-sample", async (c) => {
   const { title } = await c.req.json();
   const openai = new OpenAI({
@@ -190,27 +216,6 @@ app.post("/api/examples/create/character-sample", async (c) => {
     input: title,
   });
   return c.json({ outputText: response.output_text });
-});
-
-app.post("/api/examples/create/reasoning", async (c) => {
-  const { topic, effort } = await c.req.json();
-  const openai = new OpenAI({
-    apiKey: c.env.CLOUDFLARE_AUTH_TOKEN,
-    baseURL: c.env.BASE_URL,
-  });
-
-  const response = await openai.responses.create({
-    model: MODEL_NAME,
-    instructions: `You help plan educational lessons.
-    The user will tell you they are trying to learn.
-    Your job is to create a detailed list of prerequisite skills and information, the user should ensure they know.
-    `,
-    input: topic,
-    reasoning: {
-      effort,
-    },
-  });
-  return c.json({ response, outputText: response.output_text });
 });
 
 app.post("/api/examples/parse/relationships", async (c) => {
